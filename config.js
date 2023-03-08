bdf0226664b79be7aa4353800ec26d2777b6cf1f
NguyenNguyenNguyenPhong <109236721+NguyenNguyenNguyenPhong@users.noreply.github.com>
2023-03-08 21:47:05 +0700

    Final_Phuong_08_03

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Phượng';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 08-03';
+const titleCard = 'Tặng bạn Phượng';
+const contentCard = 'Chúc bạn có một ngày giống như chính bạn vậy, cực kì đặc biệt. 8/3 vui vẻ, xinh đẹp và luôn hạnh phúc nhé cô gái!';
+
+
+const giftImage = 'Phuong.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
